b26dfbc328b1c85b9d4e713e9c23a8288409d582
CKURZ <ckurz098@gmail.com>
2022-12-05 12:14:04 -0800

    Basic player movement demo added.

--- src/components/GameWindow/gamewindow.component.jsx
+++ src/components/GameWindow/gamewindow.component.jsx
@@ -4,13 +4,13 @@ import setKeyListeners from "../../utils/gameWindowUtils/setKeyListeners";
 import './gameWindowStyle.css'
 
 import Player from "../Player/player.component"
-const GameWindow = ({gameDim}) => {
+const GameWindow = ({ gameDim }) => {
     const [gameTicker, setGameTicker] = useState(0);
     const [screenScale, setScreenScale] = useState(window.innerWidth / gameDim.w);
-    const [player, setPlayer] = useState({x: 0, y: 0})
+    const [player, setPlayer] = useState({ w: 50 ,h: 50,x: 0, y: 0, dir: 0, spdA: 5, spdB: 10,  animate: 'paused' })
     const [kbCheck, setKbCheck] = useState([]);
     let borderWidth = (window.innerWidth - (screenScale * gameDim.w)) / 2; //calcualtes the width of the left and right black bars
-   
+
 
     useEffect(() => {
         setKeyListeners(setKbCheck);
@@ -23,25 +23,44 @@ const GameWindow = ({gameDim}) => {
     }, [])
 
     useEffect(() => {
-       //Game logic goes here
-       console.log(kbCheck)
-       if (kbCheck.includes('arrowright'||'arrowleft'))
-       {
-       setPlayer(old => {
-         if (kbCheck.includes('arrowright')) {
-            return (player.x>gameDim.w) ? {x:-250} : {x: old.x+5}
-         } 
-        else {
-            
-        return (player.x<gameDim.w-250) ? {x:gameDim.w-1} : {x: old.x-5}
-        }
-       }) }
+        //Game logic goes here
+        //console.log(kbCheck)
+
+        //--Player Movement--//
+        setPlayer(old => {
+            let {spdA: spd,w,h,x,y} = old;
+            const change = {animate: 'paused'};
+            let hDir = 0;
+            let vDir = 0;
+            if (kbCheck.includes('shift')) { spd = old.spdB }
+            if (kbCheck.includes('arrowright')) { hDir = 1; (x > gameDim.w) ? change.x = -w : change.x = old.x + spd; }
+            if (kbCheck.includes('arrowleft')) { hDir = -1; (x < -(w)) ? change.x = gameDim.w - 1 : change.x = old.x - spd; }
+            if (kbCheck.includes('arrowup')) { vDir = -1; (y < -(h)) ? change.y = gameDim.h - 1 : change.y = old.y - spd; }
+            if (kbCheck.includes('arrowdown')) { vDir = 1; (y > gameDim.h) ? change.y = -h : change.y = old.y + spd; }
+
+            if (hDir || vDir) {change.animate = 'running'}
+
+            switch (true) {
+                case (hDir === 1 && !vDir): change.dir = 0; break;
+                case (hDir === -1 && !vDir): change.dir = 180; break;
+                case (!hDir && vDir === 1): change.dir = 270; break;
+                case (!hDir && vDir === -1): change.dir = 90; break;
+                case (hDir === 1 && vDir === 1): change.dir = 315; break;
+                case (hDir === 1 && vDir === -1): change.dir = 45; break;
+                case (hDir === -1 && vDir === -1): change.dir = 135; break;
+                case (hDir === -1 && vDir === 1): change.dir = 225; break;
+                default:
+            }
+            return { ...old, ...change };
+        });
+
     }, [gameTicker])
 
     return (
         <div className="game-container">
-            <div className="game-window" style={{transform: `scale(${screenScale})`, left: `${borderWidth}px`, width: `${gameDim.w}px`, height: `${gameDim.h}px` }}>
-              <Player player = {player} setPlayer = {setPlayer}/>
+            <div className="game-window" style={{ transform: `scale(${screenScale})`, left: `${borderWidth}px`, width: `${gameDim.w}px`, height: `${gameDim.h}px` }}>
+                <Player player={player} setPlayer={setPlayer}/>
+
             </div>
         </div>
     )
