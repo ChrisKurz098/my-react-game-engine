7f744af04b2be3ba642cab70c72298ba277d3405
CKURZ <ckurz098@gmail.com>
2022-12-05 15:56:47 -0800

    created 8 way movement utility

--- src/components/GameWindow/gamewindow.component.jsx
+++ src/components/GameWindow/gamewindow.component.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import { checkScreenScale } from "../../utils/gameWindowUtils/checkScreenScale";
 import setKeyListeners from "../../utils/gameWindowUtils/setKeyListeners";
+import eightWayMovement from "../../utils/playerUtils/eightWayMovement";
 import './gameWindowStyle.css'
 
 import Player from "../Player/player.component"
 const GameWindow = ({ gameDim }) => {
     const [gameTicker, setGameTicker] = useState(0);
     const [screenScale, setScreenScale] = useState(window.innerWidth / gameDim.w);
-    const [player, setPlayer] = useState({ w: 50 ,h: 50,x: 0, y: 0, dir: 0, fall: 0, spdA: 5, spdB: 10,  scale: 1 })
+    const [player, setPlayer] = useState({ w: 50, h: 50, x: 0, y: 0, dir: 0, jump: 0, spdA: 5, spdB: 10, scale: 1 })
     const [kbCheck, setKbCheck] = useState([]);
     let borderWidth = (window.innerWidth - (screenScale * gameDim.w)) / 2; //calcualtes the width of the left and right black bars
 
@@ -24,46 +25,16 @@ const GameWindow = ({ gameDim }) => {
     useEffect(() => {
         checkScreenScale(gameDim, setScreenScale);
         //---------Game logic goes here---------//
-
+        console.log(kbCheck)
         //--Player Movement--//
-        setPlayer(old => {
-            let {spdA: spd,w,h,x,y, scale, fall} = old;
-            const change = {};
-            let hDir = 0;
-            let vDir = 0;
-            const jumpSpeed = .1;
-            if (kbCheck.includes(' ')) { change.scale = scale+jumpSpeed } else {change.scale = (scale>1) ? scale-jumpSpeed : 1}
-            
-            if (kbCheck.includes('shift')) { spd = old.spdB }
-            if (kbCheck.includes('arrowright')) { hDir = 1; (x > gameDim.w) ? change.x = -w : change.x = old.x + spd; }
-            if (kbCheck.includes('arrowleft')) { hDir = -1; (x < -(w)) ? change.x = gameDim.w - 1 : change.x = old.x - spd; }
-            if (kbCheck.includes('arrowup')) { vDir = -1; (y < -(h)) ? change.y = gameDim.h - 1 : change.y = old.y - spd; }
-            if (kbCheck.includes('arrowdown')) { vDir = 1; (y > gameDim.h) ? change.y = -h : change.y = old.y + spd; }
-           
-          
-
-            // if (hDir || vDir) {change.animate = 'running'}
-
-            switch (true) {
-                case (hDir === 1 && !vDir): change.dir = 0; break;
-                case (hDir === -1 && !vDir): change.dir = 180; break;
-                case (!hDir && vDir === 1): change.dir = 270; break;
-                case (!hDir && vDir === -1): change.dir = 90; break;
-                case (hDir === 1 && vDir === 1): change.dir = 315; break;
-                case (hDir === 1 && vDir === -1): change.dir = 45; break;
-                case (hDir === -1 && vDir === -1): change.dir = 135; break;
-                case (hDir === -1 && vDir === 1): change.dir = 225; break;
-                default:
-            }
-            return { ...old, ...change };
-        });
+        eightWayMovement(gameDim,setPlayer, kbCheck);
 
     }, [gameTicker])
 
     return (
         <div className="game-container">
             <div className="game-window" style={{ transform: `scale(${screenScale})`, left: `${borderWidth}px`, width: `${gameDim.w}px`, height: `${gameDim.h}px` }}>
-                <Player player={player} setPlayer={setPlayer}/>
+                <Player player={player} setPlayer={setPlayer} />
 
             </div>
         </div>
---
+++ src/utils/playerUtils/eightWayMovement.js
@@ -0,0 +1,35 @@
+export default function eightWayMovement(gameDim,setPlayer, kbCheck) {
+    setPlayer(old => {
+        let { spdA: spd, w, h, x, y, scale, jump } = old;
+        const change = {};
+        let hDir = 0;
+        let vDir = 0;
+        const jumpSpeed = .07;
+        const maxJump = 2;
+        //--jumping--//
+        if (kbCheck.includes('z') && !jump) (change.jump = 1);
+        if (jump === 1 && scale < maxJump) change.scale = scale + jumpSpeed;
+        if (jump === 1 && scale >= maxJump) change.jump = 2;
+        if (jump === 2 && scale > 1) { change.scale = scale - jumpSpeed; };
+        if (jump === 2 && scale <= 1) { change.jump = 0; change.scale = 1 };
+
+        if (kbCheck.includes('shift')) { spd = old.spdB }
+        if (kbCheck.includes('arrowright')) { hDir = 1; (x > gameDim.w) ? change.x = -w : change.x = old.x + spd; }
+        if (kbCheck.includes('arrowleft')) { hDir = -1; (x < -(w)) ? change.x = gameDim.w - 1 : change.x = old.x - spd; }
+        if (kbCheck.includes('arrowup')) { vDir = -1; (y < -(h)) ? change.y = gameDim.h - 1 : change.y = old.y - spd; }
+        if (kbCheck.includes('arrowdown')) { vDir = 1; (y > gameDim.h) ? change.y = -h : change.y = old.y + spd; }
+
+        switch (true) {
+            case (hDir === 1 && !vDir): change.dir = 0; break;
+            case (hDir === -1 && !vDir): change.dir = 180; break;
+            case (!hDir && vDir === 1): change.dir = 270; break;
+            case (!hDir && vDir === -1): change.dir = 90; break;
+            case (hDir === 1 && vDir === 1): change.dir = 315; break;
+            case (hDir === 1 && vDir === -1): change.dir = 45; break;
+            case (hDir === -1 && vDir === -1): change.dir = 135; break;
+            case (hDir === -1 && vDir === 1): change.dir = 225; break;
+            default:
+        }
+        return {...old, ...change};
+    });
+}
