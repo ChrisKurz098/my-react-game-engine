856211c20211629d98c43fd5f4c8ba990580de88
CKURZ <ckurz098@gmail.com>
2022-12-06 11:15:53 -0800

    tank control collision is working. Need to adjust code to allow for sliding agaist walls

--- src/utils/playerUtils/tankControls.js
+++ src/utils/playerUtils/tankControls.js
@@ -12,48 +12,51 @@ export default function tankControls(gameDim, setPlayer, kbCheck, params) {
         if (jump === 2 && scale > 1) { change.scale = scale - jumpSpeed; };
         if (jump === 2 && scale <= 1 && !kbCheck.includes('z')) { change.jump = 0; change.scale = 1 };
 
-  
+
         if (kbCheck.includes('shift')) { spd = old.spdB }
 
-          //Rotate
-          if (kbCheck.includes('arrowleft')) {
-            (dir < 360) ? change.dir = dir+turnSpeed : change.dir = 0;
+        //Rotate
+        if (kbCheck.includes('arrowleft')) {
+            (dir < 360) ? change.dir = dir + turnSpeed : change.dir = 0;
             console.log(dir)
-           }
-           if (kbCheck.includes('arrowright')) {
-               (dir <= 0) ? change.dir = 360 : change.dir = dir - turnSpeed;
-               console.log(dir)
-           }
+        }
+        if (kbCheck.includes('arrowright')) {
+            (dir <= 0) ? change.dir = 360 : change.dir = dir - turnSpeed;
+            console.log(dir)
+        }
 
         if (kbCheck.includes('arrowup')) {
             x += spd * Math.cos((change.dir || dir) * Math.PI / 180);
             y -= spd * Math.sin((change.dir || dir) * Math.PI / 180);
         }
         if (kbCheck.includes('arrowdown')) {
-          x -= spd * Math.cos((change.dir || dir) * Math.PI / 180);
-          y += spd * Math.sin((change.dir || dir) * Math.PI / 180);
+            x -= spd * Math.cos((change.dir || dir) * Math.PI / 180);
+            y += spd * Math.sin((change.dir || dir) * Math.PI / 180);
 
-      }
+        }
 
-      change.x = x;
-      change.y = y;
+        change.x = x;
+        change.y = y;
 
 
-      const walls = document.querySelectorAll('.wall');
-      walls.forEach((wall) => {
-        let { height, width, left, top } = wall.style;
-        width = parseInt(width);
-        height = parseInt(height);
-        let xx = parseInt(left); const xxx = xx + (width / 2);
-        let yy = parseInt(top); const yyy = yy + (height / 2);
-        const cx = (change.x + w / 2) ;
-        const cy = (change.y + h / 2) ;
-        const hh = (xx - cx >= 0) ? (1) : (-1);
-        const vv = (yy - cy >= 0) ? (1) : (-1);
-        if (cx >= xx && cx <= xx+width && y + (h / 2 * vv) >= yyy - height / 2 && y + (h / 2 * vv)<= yy + height / 2) change.x = old.x;
-        if (cy >= yy && cy <= yy+height && x + (w / 2 * hh) >= xxx - width / 2 && x + (w / 2 * hh)<= xx + width / 2) change.y = old.y;
+        const walls = document.querySelectorAll('.wall');
+        walls.forEach((wall) => {
+            let { height, width, left, top } = wall.style;
+            width = parseInt(width);
+            height = parseInt(height);
+            let xx = parseInt(left);
+            let yy = parseInt(top);
+            let cx = (change.x + w / 2);
+            let cy = (change.y + h / 2);
+            const hh = (xx - cx >= 0) ? (1) : (-1);
+            const vv = (yy - cy >= 0) ? (1) : (-1);
+            cx = cx + (w / 2 * hh);
+            cy = cy + (h / 2 * vv);
+            if (cx  >= xx && cx  <= xx + width && cy >= yy && cy <= yy + height) change.x = old.x;
+            
+            if (cy >= yy && cy <= yy + height && cx  >= xx && cx  <= xx + width) change.y = old.y;
 
-      });
+        });
 
 
         return { ...old, ...change };
