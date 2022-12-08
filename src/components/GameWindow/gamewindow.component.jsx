49d1b3b689f037d81b3992b622bcda22c4d582c8
CKURZ <ckurz098@gmail.com>
2022-12-07 16:19:42 -0800

    Added styling to wall params for aditional inline styles. consider adding to all components

--- src/components/GameWindow/gamewindow.component.jsx
+++ src/components/GameWindow/gamewindow.component.jsx
@@ -10,7 +10,7 @@ import Player from "../Player/player.component"
 const GameWindow = ({ gameDim }) => {
     const [gameTicker, setGameTicker] = useState(0);
     const [screenScale, setScreenScale] = useState(window.innerWidth / gameDim.w);
-    const [player, setPlayer] = useState({ w: 50, h: 50, x: 0, y: 0, dir: 0, jump: 0, spdA: 5, spdB: 10, scale: 1 })
+    const [player, setPlayer] = useState({ w: 50, h: 50, x: 0, y: 0, dir: 0, jump: 0, momentum: .5, cSpd: 0, spdA: 5, spdB: 10, scale: 1 })
     const [kbCheck, setKbCheck] = useState([]);
     const [msCheck, setMsCheck] = useState({});
     let borderWidth = (window.innerWidth - (screenScale * gameDim.w)) / 2; //calcualtes the width of the left and right black bars
@@ -18,7 +18,7 @@ const GameWindow = ({ gameDim }) => {
         {w:100, h:100, x: 500, y: 233, sprite: 'wall.png', tile: false},
         {w:100, h:250, x: 210, y: 533, sprite: 'wall.png', tile: true, tileSize: [100,100]},
         {w:80, h:400, x: 800, y: 30, jumpOver: true},
-        {w:100, h:100, x: 100, y: 233, sprite: 'wall.png', tile: true, tileSize: [50, 50]},
+        {w:100, h:100, x: 100, y: 233, color: 'blue', styling: {border: '10px solid yellow'}}
     ]
 
     useEffect(() => {
@@ -35,8 +35,7 @@ const GameWindow = ({ gameDim }) => {
         checkScreenScale(gameDim, setScreenScale);
         //---------Game logic goes here---------//
         //--Player Movement--//
-        tankControls(gameDim,setPlayer, kbCheck, ['z',.04, 2]);
-
+        eightWayMovement(gameDim,setPlayer, kbCheck, ['z',.04, 2]);
     }, [gameTicker])
 
     return (
